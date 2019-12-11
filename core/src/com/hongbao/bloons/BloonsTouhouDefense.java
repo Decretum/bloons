7e373e7c4b6ee6a68759ae7942b59a33d430e448
gzhang <gzhang@study.com>
2019-12-11 12:39:00 -0800

    The purchase tower menu now works

--- core/src/com/hongbao/bloons/BloonsTouhouDefense.java
+++ core/src/com/hongbao/bloons/BloonsTouhouDefense.java
@@ -10,7 +10,6 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.actions.RepeatAction;
 import com.badlogic.gdx.scenes.scene2d.actions.RunnableAction;
@@ -95,6 +94,18 @@ public class BloonsTouhouDefense implements ApplicationListener {
 		
 		ImageButton purchaseReimu = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("img/ui/reimu_box.png")))));
 		purchaseReimu.setPosition(1504, 676);
+		purchaseReimu.addCaptureListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				map.setSelectedGirl(null);
+				Girl girl = GirlFactory.createReimu();
+				if (player.canPurchaseGirl(girl)) {
+					GirlActor girlActor = new GirlActor(girl, Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+					map.setSelectedGirl(girlActor);
+					stage.addActor(girlActor);
+				}
+			}
+		});
 		stage.addActor(new RenderableImageButton(purchaseReimu, ZIndex.MENU_Z_INDEX));
 		
 		Label reimuCost = new Label(String.valueOf(GirlFactory.createReimu().getCost()), skin);
@@ -105,6 +116,18 @@ public class BloonsTouhouDefense implements ApplicationListener {
 		
 		ImageButton purchaseYukari = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("img/ui/yukari_box.png")))));
 		purchaseYukari.setPosition(1504, 604);
+		purchaseYukari.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				map.setSelectedGirl(null);
+				Girl girl = GirlFactory.createYukari();
+				if (player.canPurchaseGirl(girl)) {
+					GirlActor girlActor = new GirlActor(girl, Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+					map.setSelectedGirl(girlActor);
+					stage.addActor(girlActor);
+				}
+			}
+		});
 		stage.addActor(new RenderableImageButton(purchaseYukari, ZIndex.MENU_Z_INDEX));
 		
 		Label yukariCost = new Label(String.valueOf(GirlFactory.createYukari().getCost()), skin);
@@ -115,6 +138,18 @@ public class BloonsTouhouDefense implements ApplicationListener {
 		
 		ImageButton purchaseMarisa = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("img/ui/marisa_box.png")))));
 		purchaseMarisa.setPosition(1504, 532);
+		purchaseMarisa.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				map.setSelectedGirl(null);
+				Girl girl = GirlFactory.createMarisa();
+				if (player.canPurchaseGirl(girl)) {
+					GirlActor girlActor = new GirlActor(girl, Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+					map.setSelectedGirl(girlActor);
+					stage.addActor(girlActor);
+				}
+			}
+		});
 		stage.addActor(new RenderableImageButton(purchaseMarisa, ZIndex.MENU_Z_INDEX));
 		
 		Label marisaCost = new Label(String.valueOf(GirlFactory.createMarisa().getCost()), skin);
@@ -125,6 +160,18 @@ public class BloonsTouhouDefense implements ApplicationListener {
 		
 		ImageButton purchaseAlice = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("img/ui/alice_box.png")))));
 		purchaseAlice.setPosition(1504, 460);
+		purchaseAlice.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				map.setSelectedGirl(null);
+				Girl girl = GirlFactory.createAlice();
+				if (player.canPurchaseGirl(girl)) {
+					GirlActor girlActor = new GirlActor(girl, Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+					map.setSelectedGirl(girlActor);
+					stage.addActor(girlActor);
+				}
+			}
+		});
 		stage.addActor(new RenderableImageButton(purchaseAlice, ZIndex.MENU_Z_INDEX));
 		
 		Label aliceCost = new Label(String.valueOf(GirlFactory.createAlice().getCost()), skin);
@@ -135,6 +182,18 @@ public class BloonsTouhouDefense implements ApplicationListener {
 		
 		ImageButton purchaseSakuya = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("img/ui/sakuya_box.png")))));
 		purchaseSakuya.setPosition(1504, 388);
+		purchaseSakuya.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				map.setSelectedGirl(null);
+				Girl girl = GirlFactory.createSakuya();
+				if (player.canPurchaseGirl(girl)) {
+					GirlActor girlActor = new GirlActor(girl, Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+					map.setSelectedGirl(girlActor);
+					stage.addActor(girlActor);
+				}
+			}
+		});
 		stage.addActor(new RenderableImageButton(purchaseSakuya, ZIndex.MENU_Z_INDEX));
 		
 		Label sakuyaCost = new Label(String.valueOf(GirlFactory.createSakuya().getCost()), skin);
@@ -145,6 +204,18 @@ public class BloonsTouhouDefense implements ApplicationListener {
 		
 		ImageButton purchaseRemilia = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("img/ui/remilia_box.png")))));
 		purchaseRemilia.setPosition(1504, 316);
+		purchaseRemilia.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				map.setSelectedGirl(null);
+				Girl girl = GirlFactory.createRemilia();
+				if (player.canPurchaseGirl(girl)) {
+					GirlActor girlActor = new GirlActor(girl, Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+					map.setSelectedGirl(girlActor);
+					stage.addActor(girlActor);
+				}
+			}
+		});
 		stage.addActor(new RenderableImageButton(purchaseRemilia, ZIndex.MENU_Z_INDEX));
 		
 		Label remiliaCost = new Label(String.valueOf(GirlFactory.createRemilia().getCost()), skin);
@@ -155,6 +226,18 @@ public class BloonsTouhouDefense implements ApplicationListener {
 		
 		ImageButton purchaseYoumu = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("img/ui/youmu_box.png")))));
 		purchaseYoumu.setPosition(1504, 244);
+		purchaseYoumu.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				map.setSelectedGirl(null);
+				Girl girl = GirlFactory.createYoumu();
+				if (player.canPurchaseGirl(girl)) {
+					GirlActor girlActor = new GirlActor(girl, Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+					map.setSelectedGirl(girlActor);
+					stage.addActor(girlActor);
+				}
+			}
+		});
 		stage.addActor(new RenderableImageButton(purchaseYoumu, ZIndex.MENU_Z_INDEX));
 		
 		Label youmuCost = new Label(String.valueOf(GirlFactory.createYoumu().getCost()), skin);
@@ -165,6 +248,18 @@ public class BloonsTouhouDefense implements ApplicationListener {
 		
 		ImageButton purchaseYuyuko = new ImageButton(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("img/ui/yuyuko_box.png")))));
 		purchaseYuyuko.setPosition(1504, 172);
+		purchaseYuyuko.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				map.setSelectedGirl(null);
+				Girl girl = GirlFactory.createYuyuko();
+				if (player.canPurchaseGirl(girl)) {
+					GirlActor girlActor = new GirlActor(girl, Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+					map.setSelectedGirl(girlActor);
+					stage.addActor(girlActor);
+				}
+			}
+		});
 		stage.addActor(new RenderableImageButton(purchaseYuyuko, ZIndex.MENU_Z_INDEX));
 		
 		Label yuyukoCost = new Label(String.valueOf(GirlFactory.createYuyuko().getCost()), skin);
--- core/src/com/hongbao/bloons/actors/RenderableActor.java
+++ core/src/com/hongbao/bloons/actors/RenderableActor.java
@@ -1,9 +1,19 @@
 package com.hongbao.bloons.actors;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Event;
 import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.DelayedRemovalArray;
 
 
 public abstract class RenderableActor extends Actor {
@@ -12,7 +22,7 @@ public abstract class RenderableActor extends Actor {
 	// in the game (so really, all of them) needs to extend this class so that z indexes can be done properly.
 
 	private int zIndex;
-	private Actor actor;
+	private Actor actor; // This is kind of stupid, but I want this thing to be able to accommodate many superclasses of actors. If I think of a better way to do things I'll change it.
 	public TextureRegion textureRegion;
 	
 	public int getZIndex() {
@@ -50,14 +60,814 @@ public abstract class RenderableActor extends Actor {
 	
 	@Override
 	public void draw(Batch batch, float parentAlpha) {
-		actor.draw(batch, parentAlpha);
+		if (actor == null) {
+			super.draw(batch, parentAlpha);
+		} else {
+			actor.draw(batch, parentAlpha);
+		}
 	}
 	
 	@Override
 	public void act(float delta) {
-		actor.act(delta);
+		if (actor == null) {
+			super.act(delta);
+		} else {
+			actor.act(delta);
+		}
 	}
 	
+	@Override
+	public boolean fire(Event event) {
+		if (actor == null) {
+			return super.fire(event);
+		} else {
+			return actor.fire(event);
+		}
+	}
+	
+	@Override
+	public void addAction(Action action) {
+		if (actor == null) {
+			super.addAction(action);
+		} else {
+			actor.addAction(action);
+		}
+	}
+	
+	@Override
+	public void removeAction(Action action) {
+		if (actor == null) {
+			super.removeAction(action);
+		} else {
+			actor.removeAction(action);
+		}
+	}
+	
+	@Override
+	public Array<Action> getActions() {
+		if (actor == null) {
+			return super.getActions();
+		} else {
+			return actor.getActions();
+		}
+	}
+	
+	@Override
+	public boolean hasActions() {
+		if (actor == null) {
+			return super.hasActions();
+		} else {
+			return actor.hasActions();
+		}
+	}
+	
+	@Override
+	public void clearActions() {
+		if (actor == null) {
+			super.clearActions();
+		} else {
+			actor.clearActions();
+		}
+	}
+	
+	@Override
+	public void clearListeners() {
+		if (actor == null) {
+			super.clearListeners();
+		} else {
+			actor.clearListeners();
+		}
+	}
+	
+	@Override
+	public void clear() {
+		if (actor == null) {
+			super.clear();
+		} else {
+			actor.clear();
+		}
+	}
+	
+	@Override
+	public Stage getStage() {
+		if (actor == null) {
+			return super.getStage();
+		} else {
+			return actor.getStage();
+		}
+	}
+	
+	@Override
+	public boolean isDescendantOf(Actor actor) {
+		if (this.actor == null) {
+			return super.isDescendantOf(actor);
+		} else {
+			return this.actor.isDescendantOf(actor);
+		}
+	}
+	
+	@Override
+	public boolean isAscendantOf(Actor actor) {
+		if (this.actor == null) {
+			return super.isAscendantOf(actor);
+		} else {
+			return this.actor.isAscendantOf(actor);
+		}
+	}
+	
+	@Override
+	public <T extends Actor> T firstAscendant(Class<T> type) {
+		if (actor == null) {
+			return super.firstAscendant(type);
+		} else {
+			return actor.firstAscendant(type);
+		}
+	}
+	
+	@Override
+	public boolean hasParent() {
+		if (actor == null) {
+			return super.hasParent();
+		} else {
+			return actor.hasParent();
+		}
+	}
+	
+	@Override
+	public Group getParent() {
+		if (actor == null) {
+			return super.getParent();
+		} else {
+			return actor.getParent();
+		}
+	}
+	
+	@Override
+	public boolean isTouchable() {
+		if (actor == null) {
+			return super.isTouchable();
+		} else {
+			return actor.isTouchable();
+		}
+	}
+	
+	@Override
+	public Touchable getTouchable() {
+		if (actor == null) {
+			return super.getTouchable();
+		} else {
+			return actor.getTouchable();
+		}
+	}
+	
+	@Override
+	public void setTouchable(Touchable touchable) {
+		if (actor == null) {
+			super.setTouchable(touchable);
+		} else {
+			actor.setTouchable(touchable);
+		}
+	}
+	
+	@Override
+	public boolean isVisible() {
+		if (actor == null) {
+			return super.isVisible();
+		} else {
+			return actor.isVisible();
+		}
+	}
+	
+	@Override
+	public void setVisible(boolean visible) {
+		if (actor == null) {
+			super.setVisible(visible);
+		} else {
+			actor.setVisible(visible);
+		}
+	}
+	
+	@Override
+	public boolean ancestorsVisible() {
+		if (actor == null) {
+			return super.ancestorsVisible();
+		} else {
+			return actor.ancestorsVisible();
+		}
+	}
+	
+	@Override
+	public boolean hasKeyboardFocus() {
+		if (actor == null) {
+			return super.hasKeyboardFocus();
+		} else {
+			return actor.hasKeyboardFocus();
+		}
+	}
+	
+	@Override
+	public boolean hasScrollFocus() {
+		if (actor == null) {
+			return super.hasScrollFocus();
+		} else {
+			return actor.hasScrollFocus();
+		}
+	}
+	
+	@Override
+	public boolean isTouchFocusTarget() {
+		if (actor == null) {
+			return super.isTouchFocusTarget();
+		} else {
+			return actor.isTouchFocusTarget();
+		}
+	}
+	
+	@Override
+	public boolean isTouchFocusListener() {
+		if (actor == null) {
+			return super.isTouchFocusListener();
+		} else {
+			return actor.isTouchFocusListener();
+		}
+	}
+	
+	@Override
+	public Object getUserObject() {
+		if (actor == null) {
+			return super.getUserObject();
+		} else {
+			return actor.getUserObject();
+		}
+	}
+	
+	@Override
+	public void setUserObject(Object userObject) {
+		if (actor == null) {
+			super.setUserObject(userObject);
+		} else {
+			actor.setUserObject(userObject);
+		}
+	}
+	
+	@Override
+	public float getX() {
+		if (actor == null) {
+			return super.getX();
+		} else {
+			return actor.getX();
+		}
+	}
+	
+	@Override
+	public float getX(int alignment) {
+		if (actor == null) {
+			return super.getX(alignment);
+		} else {
+			return actor.getX(alignment);
+		}
+	}
+	
+	@Override
+	public void setX(float x) {
+		if (actor == null) {
+			super.setX(x);
+		} else {
+			actor.setX(x);
+		}
+	}
+	
+	@Override
+	public void setX(float x, int alignment) {
+		if (actor == null) {
+			super.setX(x, alignment);
+		} else {
+			actor.setX(x, alignment);
+		}
+	}
+	
+	@Override
+	public float getY() {
+		if (actor == null) {
+			return super.getY();
+		} else {
+			return actor.getY();
+		}
+	}
+	
+	@Override
+	public void setY(float y) {
+		if (actor == null) {
+			super.setY(y);
+		} else {
+			actor.setY(y);
+		}
+	}
+	
+	@Override
+	public void setY(float y, int alignment) {
+		if (actor == null) {
+			super.setY(y, alignment);
+		} else {
+			actor.setY(y, alignment);
+		}
+	}
+	
+	@Override
+	public float getY(int alignment) {
+		if (actor == null) {
+			return super.getY(alignment);
+		} else {
+			return actor.getY(alignment);
+		}
+	}
+	
+	@Override
+	public void setPosition(float x, float y) {
+		if (actor == null) {
+			super.setPosition(x, y);
+		} else {
+			actor.setPosition(x, y);
+		}
+	}
+	
+	@Override
+	public void setPosition(float x, float y, int alignment) {
+		if (actor == null) {
+			super.setPosition(x, y, alignment);
+		} else {
+			actor.setPosition(x, y, alignment);
+		}
+	}
+	
+	@Override
+	public void moveBy(float x, float y) {
+		if (actor == null) {
+			super.moveBy(x, y);
+		} else {
+			actor.moveBy(x, y);
+		}
+	}
+	
+	@Override
+	public float getWidth() {
+		if (actor == null) {
+			return super.getWidth();
+		} else {
+			return actor.getWidth();
+		}
+	}
+	
+	@Override
+	public void setWidth(float width) {
+		if (actor == null) {
+			super.setWidth(width);
+		} else {
+			actor.setWidth(width);
+		}
+	}
+	
+	@Override
+	public float getHeight() {
+		if (actor == null) {
+			return super.getHeight();
+		} else {
+			return actor.getHeight();
+		}
+	}
+	
+	@Override
+	public void setHeight(float height) {
+		if (actor == null) {
+			super.setHeight(height);
+		} else {
+			actor.setHeight(height);
+		}
+	}
+	
+	@Override
+	public float getTop() {
+		if (actor == null) {
+			return super.getTop();
+		} else {
+			return actor.getTop();
+		}
+	}
+	
+	@Override
+	public float getRight() {
+		if (actor == null) {
+			return super.getRight();
+		} else {
+			return actor.getRight();
+		}
+	}
+	
+	@Override
+	public void setSize(float width, float height) {
+		if (actor == null) {
+			super.setSize(width, height);
+		} else {
+			actor.setSize(width, height);
+		}
+	}
+	
+	@Override
+	public void sizeBy(float size) {
+		if (actor == null) {
+			super.sizeBy(size);
+		} else {
+			actor.sizeBy(size);
+		}
+	}
+	
+	@Override
+	public void sizeBy(float width, float height) {
+		if (actor == null) {
+			super.sizeBy(width, height);
+		} else {
+			actor.sizeBy(width, height);
+		}
+	}
+	
+	@Override
+	public void setBounds(float x, float y, float width, float height) {
+		if (actor == null) {
+			super.setBounds(x, y, width, height);
+		} else {
+			actor.setBounds(x, y, width, height);
+		}
+	}
+	
+	@Override
+	public float getOriginX() {
+		if (actor == null) {
+			return super.getOriginX();
+		} else {
+			return actor.getOriginX();
+		}
+	}
+	
+	@Override
+	public void setOriginX(float originX) {
+		if (actor == null) {
+			super.setOriginX(originX);
+		} else {
+			actor.setOriginX(originX);
+		}
+	}
+	
+	@Override
+	public float getOriginY() {
+		if (actor == null) {
+			return super.getOriginY();
+		} else {
+			return actor.getOriginY();
+		}
+	}
+	
+	@Override
+	public void setOriginY(float originY) {
+		if (actor == null) {
+			super.setOriginY(originY);
+		} else {
+			actor.setOriginY(originY);
+		}
+	}
+	
+	@Override
+	public void setOrigin(float originX, float originY) {
+		if (actor == null) {
+			super.setOrigin(originX, originY);
+		} else {
+			actor.setOrigin(originX, originY);
+		}
+	}
+	
+	@Override
+	public void setOrigin(int alignment) {
+		if (actor == null) {
+			super.setOrigin(alignment);
+		} else {
+			actor.setOrigin(alignment);
+		}
+	}
+	
+	@Override
+	public float getScaleX() {
+		if (actor == null) {
+			return super.getScaleX();
+		} else {
+			return actor.getScaleX();
+		}
+	}
+	
+	@Override
+	public void setScaleX(float scaleX) {
+		if (actor == null) {
+			super.setScaleX(scaleX);
+		} else {
+			actor.setScaleX(scaleX);
+		}
+	}
+	
+	@Override
+	public float getScaleY() {
+		if (actor == null) {
+			return super.getScaleY();
+		} else {
+			return actor.getScaleY();
+		}
+	}
+	
+	@Override
+	public void setScaleY(float scaleY) {
+		if (actor == null) {
+			super.setScaleY(scaleY);
+		} else {
+			actor.setScaleY(scaleY);
+		}
+	}
+	
+	@Override
+	public void setScale(float scaleXY) {
+		if (actor == null) {
+			super.setScale(scaleXY);
+		} else {
+			actor.setScale(scaleXY);
+		}
+	}
+	
+	@Override
+	public void setScale(float scaleX, float scaleY) {
+		if (actor == null) {
+			super.setScale(scaleX, scaleY);
+		} else {
+			actor.setScale(scaleX, scaleY);
+		}
+	}
+	
+	@Override
+	public void scaleBy(float scale) {
+		if (actor == null) {
+			super.scaleBy(scale);
+		} else {
+			actor.scaleBy(scale);
+		}
+	}
+	
+	@Override
+	public void scaleBy(float scaleX, float scaleY) {
+		if (actor == null) {
+			super.scaleBy(scaleX, scaleY);
+		} else {
+			actor.scaleBy(scaleX, scaleY);
+		}
+	}
+	
+	@Override
+	public float getRotation() {
+		if (actor == null) {
+			return super.getRotation();
+		} else {
+			return actor.getRotation();
+		}
+	}
+	
+	@Override
+	public void setRotation(float degrees) {
+		if (actor == null) {
+			super.setRotation(degrees);
+		} else {
+			actor.setRotation(degrees);
+		}
+	}
+	
+	@Override
+	public void rotateBy(float amountInDegrees) {
+		if (actor == null) {
+			super.rotateBy(amountInDegrees);
+		} else {
+			actor.rotateBy(amountInDegrees);
+		}
+	}
+	
+	@Override
+	public void setColor(Color color) {
+		if (actor == null) {
+			super.setColor(color);
+		} else {
+			actor.setColor(color);
+		}
+	}
+	
+	@Override
+	public void setColor(float r, float g, float b, float a) {
+		if (actor == null) {
+			super.setColor(r, g, b, a);
+		} else {
+			actor.setColor(r, g, b, a);
+		}
+	}
+	
+	@Override
+	public Color getColor() {
+		if (actor == null) {
+			return super.getColor();
+		} else {
+			return actor.getColor();
+		}
+	}
+	
+	@Override
+	public String getName() {
+		if (actor == null) {
+			return super.getName();
+		} else {
+			return actor.getName();
+		}
+	}
+	
+	@Override
+	public void setName(String name) {
+		if (actor == null) {
+			super.setName(name);
+		} else {
+			actor.setName(name);
+		}
+	}
+	
+	@Override
+	public void toFront() {
+		if (actor == null) {
+			super.toFront();
+		} else {
+			actor.toFront();
+		}
+	}
+	
+	@Override
+	public void toBack() {
+		if (actor == null) {
+			super.toBack();
+		} else {
+			actor.toBack();
+		}
+	}
+	
+	@Override
+	public boolean clipBegin() {
+		if (actor == null) {
+			return super.clipBegin();
+		} else {
+			return actor.clipBegin();
+		}
+	}
+	
+	@Override
+	public boolean clipBegin(float x, float y, float width, float height) {
+		if (actor == null) {
+			return super.clipBegin(x, y, width, height);
+		} else {
+			return actor.clipBegin(x, y, width, height);
+		}
+	}
+	
+	@Override
+	public void clipEnd() {
+		if (actor == null) {
+			super.clipEnd();
+		} else {
+			actor.clipEnd();
+		}
+	}
+	
+	@Override
+	public Vector2 screenToLocalCoordinates(Vector2 screenCoords) {
+		if (actor == null) {
+			return super.screenToLocalCoordinates(screenCoords);
+		} else {
+			return actor.screenToLocalCoordinates(screenCoords);
+		}
+	}
+	
+	@Override
+	public Vector2 stageToLocalCoordinates(Vector2 stageCoords) {
+		if (actor == null) {
+			return super.stageToLocalCoordinates(stageCoords);
+		} else {
+			return actor.stageToLocalCoordinates(stageCoords);
+		}
+	}
+	
+	@Override
+	public Vector2 parentToLocalCoordinates(Vector2 parentCoords) {
+		if (actor == null) {
+			return super.parentToLocalCoordinates(parentCoords);
+		} else {
+			return actor.parentToLocalCoordinates(parentCoords);
+		}
+	}
+	
+	@Override
+	public Vector2 localToScreenCoordinates(Vector2 localCoords) {
+		if (actor == null) {
+			return super.localToScreenCoordinates(localCoords);
+		} else {
+			return actor.localToScreenCoordinates(localCoords);
+		}
+	}
+	
+	@Override
+	public Vector2 localToStageCoordinates(Vector2 localCoords) {
+		if (actor == null) {
+			return super.localToStageCoordinates(localCoords);
+		} else {
+			return actor.localToStageCoordinates(localCoords);
+		}
+	}
+	
+	@Override
+	public Vector2 localToParentCoordinates(Vector2 localCoords) {
+		if (actor == null) {
+			return super.localToParentCoordinates(localCoords);
+		} else {
+			return actor.localToParentCoordinates(localCoords);
+		}
+	}
+	
+	@Override
+	public Vector2 localToAscendantCoordinates(Actor ascendant, Vector2 localCoords) {
+		if (actor == null) {
+			return super.localToAscendantCoordinates(ascendant, localCoords);
+		} else {
+			return actor.localToAscendantCoordinates(ascendant, localCoords);
+		}
+	}
+	
+	@Override
+	public Vector2 localToActorCoordinates(Actor actor, Vector2 localCoords) {
+		if (actor == null) {
+			return super.localToActorCoordinates(actor, localCoords);
+		} else {
+			return actor.localToActorCoordinates(actor, localCoords);
+		}
+	}
+	
+	@Override
+	public void drawDebug(ShapeRenderer shapes) {
+		if (actor == null) {
+			super.drawDebug(shapes);
+		} else {
+			actor.drawDebug(shapes);
+		}
+	}
+
+	@Override
+	public void setDebug(boolean enabled) {
+		if (actor == null) {
+			super.setDebug(enabled);
+		} else {
+			actor.setDebug(enabled);
+		}
+	}
+	
+	@Override
+	public boolean getDebug() {
+		if (actor == null) {
+			return super.getDebug();
+		} else {
+			return actor.getDebug();
+		}
+	}
+	
+	@Override
+	public Actor debug() {
+		if (actor == null) {
+			return super.debug();
+		} else {
+			return actor.debug();
+		}
+	}
+	
+	@Override
+	public boolean notify(Event event, boolean capture) {
+		if (actor == null) {
+			return super.notify(event, capture);
+		} else {
+			return actor.notify(event, capture);
+		}
+	}
+	
+	@Override
+	public Actor hit(float x, float y, boolean touchable) {
+		if (actor == null) {
+			return super.hit(x, y, touchable);
+		} else {
+			return actor.hit(x, y, touchable);
+		}
+	}
+
 	@Override
 	public boolean remove() {
 		if (textureRegion != null && textureRegion.getTexture() != null) {
@@ -68,7 +878,56 @@ public abstract class RenderableActor extends Actor {
 	
 	@Override
 	public boolean addListener(EventListener listener) {
-		return actor.addListener(listener);
+		if (actor == null) {
+			return super.addListener(listener);
+		} else {
+			return actor.addListener(listener);
+		}
+	}
+	
+	@Override
+	public boolean removeListener(EventListener listener) {
+		if (actor == null) {
+			return super.removeListener(listener);
+		} else {
+			return actor.removeListener(listener);
+		}
+	}
+	
+	@Override
+	public DelayedRemovalArray<EventListener> getListeners() {
+		if (actor == null) {
+			return super.getListeners();
+		} else {
+			return actor.getListeners();
+		}
+	}
+	
+	@Override
+	public boolean addCaptureListener(EventListener listener) {
+		if (actor == null) {
+			return super.addCaptureListener(listener);
+		} else {
+			return actor.addCaptureListener(listener);
+		}
+	}
+	
+	@Override
+	public boolean removeCaptureListener(EventListener listener) {
+		if (actor == null) {
+			return super.removeCaptureListener(listener);
+		} else {
+			return actor.removeCaptureListener(listener);
+		}
+	}
+	
+	@Override
+	public DelayedRemovalArray<EventListener> getCaptureListeners() {
+		if (actor == null) {
+			return super.getCaptureListeners();
+		} else {
+			return actor.getCaptureListeners();
+		}
 	}
 	
 }
